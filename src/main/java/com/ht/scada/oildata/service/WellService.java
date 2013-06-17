6370994be2f62400ff9d352a9a72aecb456f2045
赵磊 <zhaolei-dlut@163.com>
2013-06-17 14:40:02 +0800

    去掉多余的方法

--- src/main/java/com/ht/scada/oildata/service/WellService.java
+++ src/main/java/com/ht/scada/oildata/service/WellService.java
@@ -8,8 +8,7 @@ import com.ht.scada.oildata.entity.WellDGTData;
 import com.ht.scada.oildata.entity.WellData;
 
 public interface WellService {
-	
-	
+
 	/**
 	 * 获取时间段内数据
 	 * @param wellNum
@@ -59,33 +58,5 @@ public interface WellService {
 	 * @throws Exception
 	 */
 	WellDGTData geWellDGTDataByWellNumAndDatetime(String wellNum, Date time);
-	
-	
-//	/**
-//	 * 通过井号获取油井最新的产液量
-//	 * @param wellNum
-//	 * @return
-//	 * @throws Exception
-//	 * @author 赵磊
-//	 */
-//	String getLatestProductByWellNum(String wellNum) throws Exception;
-//	
-//	/**
-//	 * 通过井号获取油井最新的故障诊断信息，故障信息内容格式待定
-//	 * @param wellNum
-//	 * @return
-//	 * @throws Exception
-//	 * @author 赵磊
-//	 */
-//	FaultDiagnoseRecord getLatestWellFaultByWellNum(String wellNum) throws Exception;
-//	
-//	/**
-//	 * 通过油井编号和日期计算产液量
-//	 * @param code
-//	 * @param date
-//	 * @return
-//	 */
-//	double getDailyOilProduct(String code, Date date);
-	
-	
+		
 }
