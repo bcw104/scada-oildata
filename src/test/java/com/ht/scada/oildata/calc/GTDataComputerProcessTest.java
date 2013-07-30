e530254be9f07e601fffa0e7a98c92ab277b0b48
赵磊 <zhaolei-dlut@163.com>
2013-07-30 10:36:41 +0800

    修改测试

--- src/test/java/com/ht/scada/oildata/calc/GTDataComputerProcessTest.java
+++ src/test/java/com/ht/scada/oildata/calc/GTDataComputerProcessTest.java
@@ -51,8 +51,8 @@ public class GTDataComputerProcessTest {
         float hanShuiLiang = 0.0F;
         GTDataComputerProcess instance = new GTDataComputerProcess();
         Map expResult = null;
-        Map result = instance.calcSGTData(weiyi, zaihe, chongCi, bengJing, oilDensity, hanShuiLiang);
-        assertEquals(expResult, result);
+//        Map result = instance.calcSGTData(weiyi, zaihe, chongCi, bengJing, oilDensity, hanShuiLiang);
+//        assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
     }
